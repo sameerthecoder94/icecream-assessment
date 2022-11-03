86fa52d4ebf44a160dbfba9f50918900f10692cb
sameerthecoder94 <sameerthecoder@gmail.com>
2022-11-03 17:35:21 -0400

    Connection to the API End point The connection from axios library to the endpoint of the YELP API is Successfull

--- src/axiosBase.js
+++ src/axiosBase.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
+import config from './config';
 
-const url = '';
+const url = config.baseURL;
 
 const axiosBase = axios.create({
   baseURL: url,
+  headers: {
+    Authorization: 'Bearer ' + config.bearerAuthroizaion,
+    'Access-Control-Allow-Origin': '*',
+  },
 });
 
 export default axiosBase;
